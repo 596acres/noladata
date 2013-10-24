2be0aa12a58cb1105c90ab18cb0ad53827cda6cf
Eric Brelsford <ebrelsford@gmail.com>
2013-10-24 12:42:06 -0400

    Management command for loading noladata

---
+++ noladata/management/commands/loadnoladata.py
@@ -0,0 +1,22 @@
+from importlib import import_module
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    args = 'dataset_name'
+    help = 'Loads NOLA data (buildings, parcels, ...)'
+
+    datasets = {
+        'buildings': 'noladata.buildings',
+        'parcels': 'noladata.parcels',
+    }
+
+    def handle(self, dataset_name, *args, **options):
+        try:
+            load_module = import_module('%s.load' % self.datasets[dataset_name])
+            load_module.from_shapefile()
+        except KeyError:
+            raise CommandError('Could not find dataset %s' % dataset_name)
+        except Exception:
+            raise CommandError('There was a problem loading data')
