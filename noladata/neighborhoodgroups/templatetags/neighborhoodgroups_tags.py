8f6abc823f6ac1ed435d4c842d2583010a56e787
Eric Brelsford <ebrelsford@gmail.com>
2014-03-24 13:42:30 -0400

    Use new inplace.boundaries tag

--- noladata/neighborhoodgroups/templatetags/neighborhoodgroups_tags.py
+++ noladata/neighborhoodgroups/templatetags/neighborhoodgroups_tags.py
@@ -1,17 +1,11 @@
 from django import template
 
-from classytags.arguments import Argument
-from classytags.core import Options
-from classytags.helpers import AsTag
+from inplace.boundaries.templatetags.boundaries_tags import (BaseAllBoundariesTag)
 
 from ..models import NeighborhoodGroup
 
 
-class GetNeighborhoodGroups(AsTag):
-    options = Options(
-        'as',
-        Argument('varname', required=True, resolve=False),
-    )
+class GetNeighborhoodGroups(BaseAllBoundariesTag):
 
     def get_value(self, context):
         return NeighborhoodGroup.objects.all().order_by('label')
